c923e6259ae2737b6705a90b09d3cc2388a09a41
Dumitru Cantea <canteadumitru@gmail.com>
2018-08-15 07:22:35 +0300

    export module interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,65 @@
 
+export {
+    ArticleContent,
+    ArticleContentRef,
+    ArticleContentRefType,
+    BuildArticleContentParams,
+} from './entities/article-content';
 
+export {
+    ArticleContentBuilder,
+} from './entities/article-content-builder';
+
+export {
+    BuildNewsEventParams,
+    NewsEvent,
+    NewsEventItem,
+    NewsEventSource,
+    NewsEventStatus,
+} from './entities/event';
+
+export {
+    EventHelper,
+} from './entities/event-helper';
+
+export {
+    BuildNewsParams,
+    NewsItem,
+} from './entities/news';
+
+export {
+    NewsHelper,
+} from './entities/news-helper';
+
+export {
+    Topic,
+    TopicLocationMap,
+    TopicType,
+} from './entities/topic';
+
+export {
+    ArticleContentRepository,
+} from './interactors/article-content-repository';
+
+export {
+    CountEventsByTopicQueryParams,
+    CountEventsQueryParams,
+    EventRepository,
+    EventsQueryParams,
+    LatestEventsByTopicQueryParams,
+    LatestEventsQueryParams,
+} from './interactors/event-repository';
+
+export {
+    CountNewsByEventQueryParams,
+    CountNewsBySourceQueryParams,
+    CountNewsByTopicQueryParams,
+    CountNewsQueryParams,
+    LatestNewsByEventQueryParams,
+    LatestNewsBySourceQueryParams,
+    LatestNewsByTopicQueryParams,
+    LatestNewsQueryParams,
+    NewsQueryParams,
+    NewsRepository,
+    NewsSearchParams,
+} from './interactors/news-repository';
